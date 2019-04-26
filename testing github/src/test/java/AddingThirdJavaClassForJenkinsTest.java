0ac37a5484195d31c9c056154aebe7840a01390a
sonamsharma <sonamsharma@gmail.com>
2019-04-27 00:32:21 +0530

    Third java class for testing jenkins

---
+++ testing github/src/test/java/AddingThirdJavaClassForJenkinsTest.java
@@ -0,0 +1,16 @@
+public class AddingThirdJavaClassForJenkinsTest {
+    public static void main(String args[]){
+        //loop counter initialisation
+        int i=1;
+
+        //print statement
+        System.out.println("Output is : ");
+
+        //loop to print 1 to 50.
+        while(i<=50)
+        {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
